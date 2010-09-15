034efc681d2685b1997d42039ab5a35004b5efcf
Timo Myyrä <timo.myyra@gmail.com>
2010-09-15 15:45:41 +0300

    Remove unused files.

--- src/tursas/engine.clj
+++
@@ -1,7 +0,0 @@
-(defn supported-options
-  "Prints the supported options for Tursas."
-  []
-  (map #(str "option name " % "\n")
-         '("Hash type spin default 1 min 1 max 32"
-           "UCI_EngineAbout type string default Tursas by Timo Myyrä, see www.wickedbsd.net/projects/tursas")))
-
--- src/tursas/test.clj
+++
@@ -1,3 +0,0 @@
-(defstruct board :w-pieces :b-pieces
-           :w-pawns :w-knights :w-bishops :w-rooks :w-queens :w-kings
-           :b-pawns :b-knights :b-bishops :b-rooks :b-queens :b-kings)
