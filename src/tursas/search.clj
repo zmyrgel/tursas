fc0da3fd218740dab4d830b2fbd517f5c1842306
zmyrgel <timo.myyra@gmail.com>
2010-10-28 10:42:54 +0300

    use cons cells instead of record for now

--- src/tursas/search.clj
+++ src/tursas/search.clj
@@ -4,15 +4,11 @@
 ;;; Based on 'Why Functional Programming Matters' paper
 ;;; http://www.cse.chalmers.se/~rjmh/Papers/whyfp.pdf
 
-;; leaf = game position
-;; subtree = all possible game positions achievable with a single move from leaf
-(defrecord Node [leaf subtree])
-
 (defn moves
   "Generates list of nodes representing possible game states reachable
   from given gametree NODE."
   [node]
-  (map #(Node. % nil) (legal-states (:leaf node))))
+  (map #(cons % nil) (legal-states (first node))))
 
 (defn maptree
   "Apply f to all elements in form."
@@ -22,41 +18,40 @@
 (defn tree-node?
   "Checks if X is tree node or not."
   [x]
-  (instance? x Node))
+  (instance? x (list? node)))
 
 (defn gametree
   "Create a full gametree from STATE.
    For obvious reasons, this need to be lazy."
   [state]
-  (tree-seq tree-node? moves (Node. state nil)))
+  (tree-seq tree-node? moves (cons state nil)))
 
 (defn static
   "Evaluates the given gametree NODE."
   [node]
-  (evaluate-state (:leaf node)))
+  (evaluate-state (first node)))
 
 (defn maximise
   "Searches the maximum score from subtree"
   [node]
-  (if (nil? (:subtree node))
-    (:leat node)
-    #(max (map minimise (:subtree node)))))
+  (if (nil? (rest node))
+    (first node)
+    #(max (map minimise (rest node)))))
 
 (defn minimise
   "Searches the minimum score from subtree"
   [node]
-  (if (nil? (:subtree node))
-    (:leaf node)
-    #(min (map maximise (:subtree node)))))
+  (if (nil? (rest node))
+    (first node)
+    #(min (map maximise (rest node)))))
 
 (defn prune
   "Prunes the results of gametree search.
    Limit the search to certain DEPTH to complete the search
    in adequote time frame."
   [depth node]
-  (assoc node (:subtree node)
-         (when (not (zero? depth))
-           (map (recur (dec depth) (:subtree node)))))))
+  (cons node (when (not (zero? depth))
+               (map (recur (dec depth) (rest node)))))))
 
 ;; evaluate = maximise . maptree static . prune 5 . gametree
 (def evaluate (comp (trampoline maximise)
