e58f66c02925034c54f61a55f782d1a2b4503c91
Timo Myyrä <timo.myyra@gmail.com>
2010-11-11 18:07:05 +0200

    fix typo

--- src/tursas/state0x88.clj
+++ src/tursas/state0x88.clj
@@ -130,7 +130,7 @@
   "Converts given algebraic representation to board index value."
   [algebraic]
   (let [file (- (int (nth algebraic 0)) 97)
-        rank (- (int (onth algebraic 1)) 48)]
+        rank (- (int (nth algebraic 1)) 48)]
     (+ (* rank 16) file)))
 
 (defn piece-value->char
