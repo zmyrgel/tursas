ba22f9a27317e1885cfa037ac9f753fad9147910
Timo Myyrä <timo.myyra@gmail.com>
2010-11-26 10:25:16 +0200

    update project file
    add license information and warn-on-reflection option

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject tursas "0.1"
   :description "Chess AI engine supporting UCI and XBoard protocol."
+  :license {:name "ISC license"
+            :url "http://opensource.org/licenses/isc-license.txt"
+            :distribution :repo}
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :main tursas.core)
