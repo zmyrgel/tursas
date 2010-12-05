23f023a5240846709d5818c233ae2ea0acf310f6
Timo Myyrä <timo.myyra@gmail.com>
2010-12-05 19:58:32 +0200

    search work
    move evaluate to bottom of file.
    Removed static function as I now the evaluation function is passed as argument
    to the evaluate

--- src/tursas/search.clj
+++ src/tursas/search.clj
@@ -1,5 +1,5 @@
 (ns tursas.search
-  (:use (tursas state eval state0x88)))
+  (:use (tursas state state0x88)))
 
 (defrecord Node [label subtree])
 
@@ -36,8 +36,7 @@
       (print "REPTREE[f a]: ")
       (prn f a)
       (println "###")
-      (Node. a
-             (lazy-seq (map (partial reptree f) (f a))))))
+      (Node. a (lazy-seq (map (partial reptree f) (f a))))))
 
 (defn- gametree [state]
   (do (println "###")
@@ -45,13 +44,6 @@
       (prn state)
       (println "###")
       (reptree moves state)))
-
-(defn- static [state]
-  (do (println "###")
-      (print "STATIC[state]:")
-      (prn state)
-      (println "###")
-      (evaluate-state state)))
 
 (declare minimise)
 (defn- maximise [node]
@@ -70,33 +62,17 @@
       (prn depth (:label node))
       (println "###")
       (Node. (:label node)
-             (lazy-seq (when (pos? depth)
-                         (map (partial prune (dec depth)) (:subtree node)))))))
+             (lazy-seq (if (pos? depth)
+                         (map (partial prune (dec depth)) (:subtree node))
+                         (when (dynamic? (:label node))
+                           (map (partial prune 0) (:subtree node))))))))
 
-(declare evaluate-with-alpha)
-(declare evaluate-with-alpha-1)
-(declare evaluate-with-alpha-2)
-(declare evaluate-with-alpha-3)
-(declare evaluate-minmax)
-(defn evaluate [depth state]
-  ;;(time (evaluate-minmax depth state))
-  (time (evaluate-with-alpha depth state))
-  ;;(evaluate-with-alpha-1 depth state)
-  ;;(evaluate-with-alpha-2 depth state)
-  ;;(evaluate-with-alpha-3 depth state)
-  ;;(time (evaluate-with-alpha-3 depth state))
-  )
-
-(defn evaluate-minmax [depth state]
-  (do (println "###")
-      (print "EVALUATE-MINMAX[depth state]:")
-      (prn depth state)
-      (println "###")
-      (->> state
-           gametree
-           (prune depth)
-           (maptree static)
-           maximise)))
+(defn evaluate-with-minmax [depth eval-fn state]
+  (->> state
+       gametree
+       (prune depth)
+       (maptree eval-fn)
+       maximise))
 
 ;;;; alpha-beta pruning version ;;;;;
 
@@ -139,11 +115,11 @@
     (:label node)
     (apply min (map maximise- (:subtree node)))))
 
-(defn evaluate-with-alpha [depth state]
+(defn evaluate-with-alpha [depth eval-fn state]
   (->> state
        gametree
        (prune depth)
-       (maptree static)
+       (maptree eval-fn)
        maximise-
        (apply max)))
 
@@ -164,11 +140,11 @@
          (sort (complement higher)
                (map highfirst (:subtree node)))))
 
-(defn evaluate-with-alpha-1 [depth state]
+(defn evaluate-with-alpha-1 [depth eval-fn state]
   (->> state
        gametree
        (prune depth)
-       (maptree static)
+       (maptree eval-fn)
        highfirst
        maximise-
        (apply max)))
@@ -181,11 +157,11 @@
 (defn taketree [n node]
   (redtree (partial nodett n) cons nil node))
 
-(defn evaluate-with-alpha-2 [depth state]
+(defn evaluate-with-alpha-2 [depth eval-fn state]
   (->> state
        gametree
        (prune depth)
-       (maptree static)
+       (maptree eval-fn)
        highfirst
        (taketree 3)
         maximise-
@@ -193,23 +169,21 @@
 
 ;;;; dynamic position consideration ;;;;;
 
-(defn- prune-new [depth node]
-  (do (println "###")
-      (print "PRUNE-NEW[depth (:label node)]:")
-      (prn depth (:label node))
-      (println "###")
-      (Node. (:label node)
-             (lazy-seq (if (pos? depth)
-                         (map (partial prune-new (dec depth)) (:subtree node))
-                         (when (dynamic? (:label node))
-                           (map (partial prune-new 0) (:subtree node))))))))
-
-(defn- evaluate-with-alpha-3 [depth state]
+(defn- evaluate-with-alpha-3 [depth eval-fn state]
   (->> state
        gametree
-       (prune-new depth)
-       (maptree static)
+       (prune depth)
+       (maptree eval-fn)
        highfirst
        (taketree 3)
        maximise-
        (apply max)))
+
+(defn evaluate [depth eval-fn state]
+  (time (evaluate-with-minmax depth eval-fn state))
+  ;;(time (evaluate-with-alpha depth eval-fn state))
+  ;;(time (evaluate-with-alpha-1 depth eval-fn state))
+  ;;(time (evaluate-with-alpha-2 depth eval-fn state))
+  ;;(time (evaluate-with-alpha-3 depth eval-fn state))
+  ;;(time (evaluate-with-alpha-3 depth eval-fn state))
+  )
