5a5084f93108c60ba7455b8712a9d673eec81331
Timo Myyrä <timo.myyra@gmail.com>
2010-12-05 20:05:57 +0200

    don't warn on reflection yet

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://opensource.org/licenses/isc-license.txt"
             :distribution :repo}
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :warn-on-reflection true
+  :warn-on-reflection false
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :main tursas.core)
