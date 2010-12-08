5cfa04f8aafa9846e1de33a3d277561af964535d
Timo Myyrä <timo.myyra@gmail.com>
2010-12-08 14:00:19 +0200

    remove io! macro from xboard and indent

--- src/tursas/xboard.clj
+++ src/tursas/xboard.clj
@@ -3,89 +3,88 @@
         (tursas game)))
 
 (def xboard-supported-features
-     {:ping 1
-      :setboard 1
-      :playother 1
-      :san 0
-      :usermove 1
-      :time 0
-      :draw 1
-      :sigint 0
-      :sigterm 0
-      :reuse 0
-      :analyse 0
-      :myname "\"Tursas 0.1\""
-      :variants "\"normal\""
-      :colors 0
-      :ics 0
-      :name 0
-      :pause 0
-      :nps 0
-      :debug 0
-      :memory 0
-      :smp 0
-      :done 1})
+  {:ping 1
+   :setboard 1
+   :playother 1
+   :san 0
+   :usermove 1
+   :time 0
+   :draw 1
+   :sigint 0
+   :sigterm 0
+   :reuse 0
+   :analyse 0
+   :myname "\"Tursas 0.1\""
+   :variants "\"normal\""
+   :colors 0
+   :ics 0
+   :name 0
+   :pause 0
+   :nps 0
+   :debug 0
+   :memory 0
+   :smp 0
+   :done 1})
 
 (defn xboard-print-supported-features
   "Prints the default features of the engine."
   []
-  (io!
-   (dorun (map println
-               (for [option (keys xboard-supported-features)]
-                 (format "feature %s=%s"
-                         (as-str option)
-                         (xboard-supported-features option)))))))
+  (dorun (map println
+              (for [option (keys xboard-supported-features)]
+                (format "feature %s=%s"
+                        (as-str option)
+                        (xboard-supported-features option))))))
 
 (defn print-xboard-usage
   "Prints the available commands of the repl."
   []
-  (io! (map-str
-        println
-        '("Available XBoard commands are:"
-          "protover N - change engine to use protocol version N"
-          "accepted - Accept last feature"
-          "reject - Reject last feature"
-          "variant VARIANT - change to use VARIANT rules. Only 'normal' supported"
-          "quit - Quit the engine"
-          "random - Tell engine to add little random elements"
-          "force - Disable engine AI"
-          "go - Enable engine AI"
-          ;;"playother - Tell AI to switch sides"
-          ;;"white - Tell white to move, engine to play black [obsolete]"
-          ;;"black - Tell black to move, engine to play white [obsolete]"
-          "level MPS BASE INC - set time controls"
-          "st TIME - set time controls"
-          "sd DEPTH - set search depth to DEPTH"
-          "nps NODE_RATE - search only NODE_RATE nodes"
-          ;;"time N - set the engine clock to N centiseconds"
-          ;;"otim N - set the opponents clock"
-          "usermove MOVE - make given MOVE if legal"
-          "? - Tell Engine to stop thinking and make its move now"
-          "ping N - Pings the engine for pong reply"
-          ;;"draw - offer draw to engine"
-          "result RESULT {COMMENTS} - give the game RESULT to engine"
-          "setboard FEN - Set the game board to given FEN."
-          ;;"edit - enable edit mode [obsolete]"
-          ;;". - exit edit mode"
-          "hint - prompt move hint from engine"
-          "bk - use book"
-          "undo - tell engine to undo last move"
-          "remove - tell engine to undo last two moves"
-          "hard - tell engine to ponder during players turn"
-          "easy - tell engine to ponder only during its turn"
-          "post - tell engine to send ponder output"
-          "nopost - tell engine not to send ponder output"
-          ;;"analyse - tell engine to engage analyse mode"
-          "name X - tell engine its opponents name"
-          "rating - ask engine its rating"
-          ;;"ics - tell engine its engaging in ICS game"
-          "computer - tell engine that its playing against cpu"
-          ;;"pause - pause all actions"
-          ;;"resume - resume all paused actions"
-          ;;"memory N - specify how much engine can use memory"
-          ;;"cores N - tell engine how many cpu cores it can use"
-          ;;"egtpath PATH - tell engine to use end-game tables from PATH"
-          "option NAME[=VALUE] - tell engine to use new option"))))
+  (map-str
+   println
+   '("Available XBoard commands are:"
+     "protover N - change engine to use protocol version N"
+     "accepted - Accept last feature"
+     "reject - Reject last feature"
+     "variant VARIANT - change to use VARIANT rules. Only 'normal' supported"
+     "quit - Quit the engine"
+     "random - Tell engine to add little random elements"
+     "force - Disable engine AI"
+     "go - Enable engine AI"
+     ;;"playother - Tell AI to switch sides"
+     ;;"white - Tell white to move, engine to play black [obsolete]"
+     ;;"black - Tell black to move, engine to play white [obsolete]"
+     "level MPS BASE INC - set time controls"
+     "st TIME - set time controls"
+     "sd DEPTH - set search depth to DEPTH"
+     "nps NODE_RATE - search only NODE_RATE nodes"
+     ;;"time N - set the engine clock to N centiseconds"
+     ;;"otim N - set the opponents clock"
+     "usermove MOVE - make given MOVE if legal"
+     "? - Tell Engine to stop thinking and make its move now"
+     "ping N - Pings the engine for pong reply"
+     ;;"draw - offer draw to engine"
+     "result RESULT {COMMENTS} - give the game RESULT to engine"
+     "setboard FEN - Set the game board to given FEN."
+     ;;"edit - enable edit mode [obsolete]"
+     ;;". - exit edit mode"
+     "hint - prompt move hint from engine"
+     "bk - use book"
+     "undo - tell engine to undo last move"
+     "remove - tell engine to undo last two moves"
+     "hard - tell engine to ponder during players turn"
+     "easy - tell engine to ponder only during its turn"
+     "post - tell engine to send ponder output"
+     "nopost - tell engine not to send ponder output"
+     ;;"analyse - tell engine to engage analyse mode"
+     "name X - tell engine its opponents name"
+     "rating - ask engine its rating"
+     ;;"ics - tell engine its engaging in ICS game"
+     "computer - tell engine that its playing against cpu"
+     ;;"pause - pause all actions"
+     ;;"resume - resume all paused actions"
+     ;;"memory N - specify how much engine can use memory"
+     ;;"cores N - tell engine how many cpu cores it can use"
+     ;;"egtpath PATH - tell engine to use end-game tables from PATH"
+     "option NAME[=VALUE] - tell engine to use new option")))
 
 (defn xboard-accept-feature
   "Tells the engine that GUI accepts last feature."
