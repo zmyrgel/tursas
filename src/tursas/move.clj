fea70be487a1df43eb015727e9e97e5c7390a29c
Timo Myyrä <timo.myyra@gmail.com>
2010-12-09 12:22:45 +0200

    strip comments from move protocol

--- src/tursas/move.clj
+++ src/tursas/move.clj
@@ -1,11 +1,5 @@
 (ns tursas.move)
 
-;;; Move data structure holds information for given move
-;;; and is presented in long algebraic notation
-;;; The long algebraic notation is used in UCI and XBoard protocols
-;;; to describe made chess moves.
-;;; Examples:  e2e4, e7e5, e1g1 (white short castling), e7e8q (for promotion)
-
 (defprotocol Move
   (move->algebraic [move])
   (from [move])
