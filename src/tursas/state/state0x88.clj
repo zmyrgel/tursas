cc6425f3ca9eecca7346c6a33bf723c7c27e7974
Timo Myyrä <timo.myyra@gmail.com>
2010-12-23 21:14:02 +0200

    minor formatting

--- src/tursas/state/state0x88.clj
+++ src/tursas/state/state0x88.clj
@@ -74,13 +74,11 @@
           player (get board TURN-STORE)
           moving-piece (get board (:from move))]
       (cond (promotion? moving-piece move)
-            (promote-piece state (:to move)
-                           (get-promotion-piece player move))
+            (promote-piece state (:to move) (get-promotion-piece player move))
             (castling? moving-piece move)
             (move-castling-pieces player state move
                                   (if (= column (:to move) 2)
-                                    QUEEN-SIDE
-                                    KING-SIDE))
+                                    QUEEN-SIDE KING-SIDE))
             :else (move-piece state move)))))
 
 (defn- update-castling
