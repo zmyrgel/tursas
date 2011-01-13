cef9a2d3a3faeb88580976a3e9d3351b7cb7b806
Timo Myyrä <timo.myyra@gmail.com>
2011-01-13 13:41:36 +0200

    refactore state0x88 a bit
    use when-not and == more

--- src/tursas/state/state0x88.clj
+++ src/tursas/state/state0x88.clj
@@ -27,16 +27,16 @@
         vals (vals pieces)
         piece-count (count keys)]
     (and (<= piece-count 4)
-         (or (= piece-count 2)
-             (and (= piece-count 3)
-                  (any? #(or (= BLACK-KNIGHT %)
-                             (= BLACK-BISHOP %)
-                             (= WHITE-KNIGHT %)
-                             (= WHITE-BISHOP %))
+         (or (== piece-count 2)
+             (and (== piece-count 3)
+                  (any? #(or (== BLACK-KNIGHT %)
+                             (== BLACK-BISHOP %)
+                             (== WHITE-KNIGHT %)
+                             (== WHITE-BISHOP %))
                         vals))
-             (and (= piece-count 4)
-                  (or (= (count (filter #(= BLACK-KNIGHT %) vals)) 2)
-                      (= (count (filter #(= WHITE-KNIGHT %) vals)) 2)
+             (and (== piece-count 4)
+                  (or (== (count (filter #(= BLACK-KNIGHT %) vals)) 2)
+                      (== (count (filter #(= WHITE-KNIGHT %) vals)) 2)
                       (let [bishops (filter #(or (= BLACK-BISHOP (get pieces %))
                                                  (= WHITE-BISHOP (get pieces %)))
                                             keys)]
@@ -48,15 +48,15 @@
     Reads the char from move or if nil, defaults to queen."
   [player move]
   (if-let [piece (:promotion move)]
-    (if (= player WHITE) (abs piece) piece)
-    (if (= player WHITE) WHITE-QUEEN BLACK-QUEEN)))
+    (if (== player WHITE) (abs piece) piece)
+    (if (== player WHITE) WHITE-QUEEN BLACK-QUEEN)))
 
 (defn- move-castling-pieces
   "Helper function for update-board to make castling move on board.
    Mainly it moves the king piece and the participating rook piece."
   [player state move castling-side]
   (let [[rook king from to]
-        (if (= player WHITE)
+        (if (== player WHITE)
           [WHITE-ROOK WHITE-KING [0x00 0x07] [0x03 0x05]]
           [BLACK-ROOK BLACK-KING [0x70 0x77] [0x73 0x75]])]
     (-> state
@@ -68,7 +68,7 @@
 (defn- update-board
   "Returns state with new board after applying MOVE to STATE."
   [move state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (let [board (:board state)
           player (int (get board TURN-STORE))
           moving-piece (int (get board (:from move)))]
@@ -76,7 +76,7 @@
             (promote-piece state (:to move) (get-promotion-piece player move))
             (castling? moving-piece move)
             (move-castling-pieces player state move
-                                  (if (= column (:to move) 2)
+                                  (if (== column (:to move) 2)
                                     QUEEN-SIDE KING-SIDE))
             :else (move-piece state move)))))
 
@@ -84,7 +84,7 @@
   "Updates states castling value for move
     checks for king or rook moves."
   [move state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (assoc state :board
            (fill-square (:board state) CASTLING-STORE
                         (let [castling (byte (get (:board state) CASTLING-STORE))]
@@ -92,20 +92,20 @@
                             0
                             (let [[k-mask qr-mask kr-mask king-sq rook-q-sq
                                    rook-k-sq opp-rkq-mask opp-rkk-mask opp-rk-q-sq opp-rk-k-sq]
-                                  (if (= (get (:board state) TURN-STORE) WHITE)
+                                  (if (== (get (:board state) TURN-STORE) WHITE)
                                     [ 3 11  7 0x04 0x00 0x07 14 13 0x70 0x77]
                                     [12 14 13 0x74 0x70 0x77 11 7 0x00 0x07])]
-                              (cond (= (:from move) king-sq) (bit-and castling k-mask)
-                                    (= (:from move) rook-q-sq) (bit-and castling qr-mask)
-                                    (= (:from move) rook-k-sq) (bit-and castling kr-mask)
-                                    (= (:to move) opp-rk-q-sq) (bit-and castling opp-rkq-mask)
-                                    (= (:to move) opp-rk-k-sq) (bit-and castling opp-rkk-mask)
+                              (cond (== (:from move) king-sq) (bit-and castling k-mask)
+                                    (== (:from move) rook-q-sq) (bit-and castling qr-mask)
+                                    (== (:from move) rook-k-sq) (bit-and castling kr-mask)
+                                    (== (:to move) opp-rk-q-sq) (bit-and castling opp-rkq-mask)
+                                    (== (:to move) opp-rk-k-sq) (bit-and castling opp-rkk-mask)
                                     :else castling))))))))
 
 (defn- update-turn
   "Updates player turn value on board."
   [state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (assoc state :board
            (fill-square (:board state) TURN-STORE
                         (opponent (get (:board state) TURN-STORE))))))
@@ -113,28 +113,28 @@
 (defn- update-en-passant
   "Associates new en-passant string with given STATE based on the MOVE."
   [move state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (assoc state :board
            (fill-square (:board state) EN-PASSANT-STORE
                         (let [piece (int (get (:board state) (:from move)))]
                           (if (and (or (= piece WHITE-PAWN)
                                        (= piece BLACK-PAWN))
-                                   (= (abs (- (:to move) (:from move))) 0x20))
+                                   (== (abs (- (:to move) (:from move))) 0x20))
                             (/ (+ (:to move) (:from move)) 2)
                             EMPTY))))))
 
 (defn- pawn-or-capture-move?
   "Predicate to see if move was pawn move or a capture"
   [board move]
-  (or (= (get board (:from move)) WHITE-PAWN)
-      (= (get board (:from move)) BLACK-PAWN)
-      (not (= (get board (:to move)) EMPTY))))
+  (or (== (get board (:from move)) WHITE-PAWN)
+      (== (get board (:from move)) BLACK-PAWN)
+      (not= (get board (:to move)) EMPTY)))
 
 (defn- update-half-moves
   "Increases half move count on board unless the move
    was pawn or a capture move."
   [move state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (assoc state :board
            (fill-square (:board state) HALF-MOVE-STORE
                         (if (pawn-or-capture-move? (:board state) move)
@@ -146,8 +146,8 @@
    XXX: Byte can only handle 128 moves, add second square to hold
         a multiplier for full-moves like 3 x 125 for move 375."
   [state]
-  (when (not (nil? state))
-    (if (= (get (:board state) TURN-STORE) BLACK)
+  (when-not (nil? state)
+    (if (== (get (:board state) TURN-STORE) BLACK)
       (assoc state :board
              (fill-square (:board state) FULL-MOVE-STORE
                           (inc (get (:board state) FULL-MOVE-STORE))))
@@ -157,7 +157,7 @@
   "Update the previous move of board.
    Stores previous move to 'off-board' locations"
   [move state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (assoc state :board
            (-> (:board state)
                (fill-square PREV-MOVE-FROM (:from move))
@@ -169,7 +169,7 @@
    Check for check condition by testing if next players king
    is threatened by the last player."
   [state]
-  (when (not (nil? state))
+  (when-not (nil? state)
     (assoc state :board
            (let [board (:board state)
                  player (get board TURN-STORE)
@@ -203,7 +203,7 @@
   (white? [state index]
     (occupied-by? (:board state) index WHITE))
   (check? [state]
-    (= (get (:board state) GAME-STATUS-STORE) CHECK-BIT))
+    (== (get (:board state) GAME-STATUS-STORE) CHECK-BIT))
   (mate? [state]
     (and (check? state)
          (empty? (legal-states state))))
@@ -216,21 +216,23 @@
   (apply-move [state move]
     (when (allowed-move? state move)
       (when-let [new-state (update-state state move)]
-        (when (not (check? new-state))
+        (when-not (check? new-state)
           new-state))))
   (legal-states [state]
     (->> state
          (pseudo-moves (get (:board state) TURN-STORE))
          (states state)))
   (turn [state]
-    (if (= (get (:board state) TURN-STORE) WHITE) :white :black))
+    (if (== (get (:board state) TURN-STORE) WHITE)
+      :white
+      :black))
   (last-move [state]
     (let [board (:board state)
           prev-piece (get board PREV-PIECE)
           piece (get board (get board PREV-MOVE-TO))]
       (make-move (get board PREV-MOVE-FROM)
                  (get board PREV-MOVE-TO)
-                 (if-not (= prev-piece piece)
+                 (if-not (== prev-piece piece)
                    piece
                    0))))
   (perft [state depth]
