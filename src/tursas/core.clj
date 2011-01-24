ed2dba46372d0b12b106b9db3f28d50ef8c10467
Timo Myyrä <timo.myyra@gmail.com>
2011-01-24 19:32:03 +0200

    rename xboard to cecp and remove uci
    UCI stuff not supported now or near future so leave it out for now to keep
    things simple.

--- src/tursas/core.clj
+++ src/tursas/core.clj
@@ -13,12 +13,12 @@
 (def inf Integer/MAX_VALUE)
 (def -inf (+ Integer/MIN_VALUE 1))
 
-(def xboard-supported-features {:ping 1, :setboard 1, :playother 1, :san 0
-                                :usermove 0, :time 0, :draw 1, :sigint 0
-                                :sigterm 0, :reuse 0, :analyse 0
-                                :myname "\"Tursas 0.1\"", :variants "\"normal\""
-                                :colors 0, :ics 0, :name 0, :pause 0, :nps 0
-                                :debug 0, :memory 0, :smp 0, :done 1})
+(def cecp-supported-features {:ping 1, :setboard 1, :playother 1, :san 0
+                              :usermove 0, :time 0, :draw 1, :sigint 0
+                              :sigterm 0, :reuse 0, :analyse 0
+                              :myname "\"Tursas 0.1\"", :variants "\"normal\""
+                              :colors 0, :ics 0, :name 0, :pause 0, :nps 0
+                              :debug 0, :memory 0, :smp 0, :done 1})
 
 (def protocol (ref :general))
 (def game-state (ref ()))
@@ -29,7 +29,7 @@
                         :time-limit 15
                         :random-mode false
                         :ai-mode false
-                        :xboard-protocol-version 1
+                        :cecp-protocol-version 1
                         :debug false
                         :ponder false
                         :ponder-output false
@@ -247,23 +247,21 @@
               (rest @game-state)
               (nthnext @game-state n)))))
 
-;; xboard
-
-(defn xboard-print-supported-features
+(defn cecp-print-supported-features
   "Prints the default features of the engine."
   []
   (dorun (map println
-              (for [option (keys xboard-supported-features)]
+              (for [option (keys cecp-supported-features)]
                 (format "feature %s=%s"
                         (s/as-str option)
-                        (xboard-supported-features option))))))
+                        (cecp-supported-features option))))))
 
-(defn print-xboard-usage
+(defn print-cecp-usage
   "Prints the available commands of the repl."
   []
   (s/map-str
    println
-   '("Available XBoard commands are:"
+   '("Available Cecp commands are:"
      "protover N - change engine to use protocol version N"
      "accepted - Accept last feature"
      "reject - Reject last feature"
@@ -309,39 +307,39 @@
      ;;"egtpath PATH - tell engine to use end-game tables from PATH"
      "option NAME[=VALUE] - tell engine to use new option")))
 
-(defn xboard-accept-feature
+(defn cecp-accept-feature
   "Tells the engine that GUI accepts last feature."
   [])
 
-(defn xboard-reject-feature
+(defn cecp-reject-feature
   "Tells the engine that GUI rejects given feature."
   [])
 
-(defn xboard-move-now
+(defn cecp-move-now
   "Tells the Engine to stop thinking and pick move immidiately."
   [])
 
-(defn xboard-ping
-  "Tells XBoard to wait for all the stuff to complete given before this
+(defn cecp-ping
+  "Tells Cecp to wait for all the stuff to complete given before this
    and once done, respond with pong"
   [n]
   (println (str "pong " n)))
 
-(defn xboard-result
+(defn cecp-result
   "Sets the game result to engine, for learning purposes
    discarded by Tursas for now."
   [result])
 
-(defn xboard-bk
-  "Tells the XBoard to use Book"
+(defn cecp-bk
+  "Tells the Cecp to use Book"
   [])
 
-(defn xboard-send-rating
+(defn cecp-send-rating
   "Prompts the Engine to send its rating."
   []
   (println "100"))
 
-(defn xboard-parse-option
+(defn cecp-parse-option
   "Wrapper to parse options from string and set them."
   [option]
   (let [pair (s/split #"=" option)]
@@ -350,14 +348,14 @@
                    true
                    (second pair)))))
 
-(defn process-xboard-command
-  "Processes command in xboard mode."
+(defn process-cecp-command
+  "Processes command in cecp mode."
   [words]
   (case (first words)
-        "protover" (do (set-option! :xboard-protocol-version (second words))
-                       (xboard-print-supported-features))
-        "accepted" (xboard-accept-feature)
-        "rejected" (xboard-reject-feature)
+        "protover" (do (set-option! :cecp-protocol-version (second words))
+                       (cecp-print-supported-features))
+        "accepted" (cecp-accept-feature)
+        "rejected" (cecp-reject-feature)
         "new" (do (set-game! "startpos")
                   (set-option! :ai-mode false))
         "variant" (set-option! :variant (second words))
@@ -366,29 +364,29 @@
         "force" (set-option! :ai-mode false)
         "go" (set-option! :ai-mode true)
         ;; set playother=1 to enable
-        ;;"playother" (xboard-playother)
+        ;;"playother" (cecp-playother)
         ;; set colors=1 to enable these
-        ;;"white" (xboard-white)
-        ;;"black" (xboard-black)
+        ;;"white" (cecp-white)
+        ;;"black" (cecp-black)
         ;;"level" (set-option! :level (rest words))
         ;;"st" (set-option! :time (second words))
         "sd" (set-option! :depth-limit (second words))
         ;;"nps" (set-option! :nps (rest words))
         ;; set time=1 to enable these
-        ;;"time" (xboard-set-engine-clock (second words))
-        ;;"otim" (xboard-set-opponent-clock (second words))
+        ;;"time" (cecp-set-engine-clock (second words))
+        ;;"otim" (cecp-set-opponent-clock (second words))
         "usermove" (make-chess-move (second words))
-        ;;"?" (xboard-move-now)
-        "ping" (xboard-ping (second words))
+        ;;"?" (cecp-move-now)
+        "ping" (cecp-ping (second words))
         ;; set draw=1 to enable
-        ;;"draw" (xboard-draw)
-        "result" (xboard-result (rest words))
+        ;;"draw" (cecp-draw)
+        "result" (cecp-result (rest words))
         ;; setboard=0 to disable setboard and use edit words
         "setboard" (set-game! (second words))
-        ;;"edit" (xboard-enter-edit-mode)
-        ;;"." (xboard-exit-edit-mode)
+        ;;"edit" (cecp-enter-edit-mode)
+        ;;"." (cecp-exit-edit-mode)
         "hint" (get-hint)
-        ;;"bk" (xboard-bk)
+        ;;"bk" (cecp-bk)
         "undo" (undo-move)
         "remove" (undo-move 2)
         ;;"hard" (set-option! :ponder true)
@@ -396,153 +394,24 @@
         ;;"post" (set-option! :ponder-output true)
         ;;"nopost" (set-option! :ponder-output false)
         ;; set analyse=1 to enable
-        ;;"analyse" (xboard-analyse-mode)
+        ;;"analyse" (cecp-analyse-mode)
         "name" (set-option! :opponent-name (second words))
-        "rating" (xboard-send-rating)
+        "rating" (cecp-send-rating)
         ;; set ics=1 to enable
-        ;;"ics" (xboard-ics)
+        ;;"ics" (cecp-ics)
         "computer" (set-option! :opponent "cpu")
         ;; set pause=1 to enable
-        ;;"pause" (xboard-pause)
-        ;;"resume" (xboard-resume)
+        ;;"pause" (cecp-pause)
+        ;;"resume" (cecp-resume)
         ;; set memory=1 to enable
-        ;;"memory" (xboard-set-memory (second words))
+        ;;"memory" (cecp-set-memory (second words))
         ;; set smp=1
-        ;;"cores" (xboard-set-cores (second words))
+        ;;"cores" (cecp-set-cores (second words))
         ;; set egtpath to enable
-        ;;"egtpath" (xboard-set-egtpath (second words))
-        "option" (xboard-parse-option (second words))
+        ;;"egtpath" (cecp-set-egtpath (second words))
+        "option" (cecp-parse-option (second words))
         (when (move-string? (first words))
           (make-chess-move (first words)))))
-
-;; uci
-(defn supported-uci-options
-  "Prints the supported options for Tursas."
-  []
-  (map #(str "option name " % "\n")
-       '("Hash type spin default 1 min 1 max 32"
-         "UCI_EngineAbout type string default Tursas by Timo Myyrä")))
-
-(defn- uci-set-position!
-  "Sets game to position [fen <fenstring> | startpos ] moves <move1> .... <movei>"
-  [pos]
-  (let [operands (re-seq #"\S+" pos)]
-    (if (= (first operands) "fen")
-      (set-game! (second operands))
-      (set-game! (first operands)))
-    (cond (and (not (nil? (nth operands 1 nil)))
-               (= (nth operands 1 nil) "moves"))
-          (map make-chess-move (nthnext operands 2))
-          (and (not (nil? (nth operands 2 nil)))
-               (= (nth operands 2 nil) "moves"))
-          (map make-chess-move (nthnext operands 3)))))
-
-(defn- register-name
-  "Registers given name."
-  [name]
-  (println "Registered name " name))
-
-(defn- register-value
-  "Registers given value."
-  [value]
-  (println "Registered value " value))
-
-(defn- register
-  "Registers values."
-  [command]
-  (let [words (re-seq #"\S+" command)]
-    (cond (= (first words) "register") (println "register got")
-          (= (first words) "name") (register-name (second words))
-          (= (first words) "code") (register-value (second words))
-          :else (println "invalid option!"))))
-
-(defn- go-handler
-  "Handler for go command"
-  [command]
-  (case (first command)
-        "searchmoves" (do (set-option! :move-limit (take-while move-string? command))
-                          (recur (drop-while move-string? command)))
-        "ponder" (do (set-option! :ponder-mode true)
-                     (recur (rest command)))
-        "wtime" (do (set-clock! :white (second command))
-                    (recur (rest command)))
-        "btime" (do (set-clock! :black (second command))
-                    (recur (rest command)))
-        "winc" (do (set-option! :white-increment (second command))
-                   (recur (rest command)))
-        "binc" (do (set-option! :black-increment (second command))
-                   (recur (rest command)))
-        "movestogo" (do (set-option! :movestogo (second command))
-                        (recur (rest command)))
-        "depth" (do (set-option! :depth-limit (second command))
-                    (recur (rest command)))
-        "nodes" (do (set-option! :node-limit (second command))
-                    (recur (rest command)))
-        "movetime" (do (set-option! :search-time (second command))
-                       (recur (rest command)))
-        "infinite" (do (set-option! :search-time 0)
-                       (recur (rest command)))
-        (println "Unknown uci command!")))
-
-(defn print-uci-usage
-  "Prints the available commands of the repl."
-  []
-  (s/map-str
-   println
-   '("Available UCI commands are:"
-     "debug [ on | off ] - print debug messages"
-     "isready - Prompts the engine if its ready"
-     "setoption name <id> [value <x>]"
-     "register [later | name <x> | code <x> ] - register values with engine"
-     "ucinewgame - start a new game"
-     "position [fen <fenstring> | startpos ] moves <move1> .... <movei>"
-     "go [searchmoves <move1> .... <movei>]"
-     "   ponder - startpos searching in pondering mode"
-     "   wtime <x> - white has x msec left on the clock"
-     "   btime <x> - black has x msec left on the clock"
-     "   winc <x> - white increment per move in mseconds if x > 0"
-     "   binc <x> - black increment per move in mseconds if x > 0"
-     "   movestogo <x> - there are x moves to next time control"
-     "   depth <x> - search x plies only"
-     "   nodes <x> - search x nodes only"
-     "   movetime <x> - search exactly x mseconds"
-     "   infinite - search until 'stop' command is sent"
-     " stop - stop calculating as soon as possible"
-     " ponderhit - the user has played the expected move.")))
-
-(defn- uci-set-option!
-  "Sets UCI specific game options:
-   name <id> [value <x>]"
-  [options]
-  (let [words (re-seq #"\S+" options)]
-    (case (count words)
-          2 (set-option! (keyword (second words)) true)
-          4 (set-option! (keyword (second words)) (nth words 4))
-          (println "Error parsing options!"))))
-
-(defn process-uci-command
-  "Processes command in uci mode."
-  [words]
-  (case (first words)
-        "uci" (do (println "id name Tursas 0.1")
-                  (println "id author Timo Myyrä")
-                  (map println (supported-uci-options))
-                  (println "uciok"))
-        "debug" (set-option! :debug
-                             (if (= (second words) "on")
-                               true
-                               false))
-        "isready" (println "readyok")
-        "setoption" (uci-set-option! (rest words))
-        "register" (register (rest words))
-        "ucinewgame" (set-game! "startpos")
-        "position" (uci-set-position! (rest words))
-        "go" (go-handler (re-seq #"\S+" (rest words)))
-        "stop"
-        "ponderhit"
-        nil))
-
-;; repl
 
 (defn- print-usage
   "Prints the available commands of the repl."
@@ -560,12 +429,12 @@
          "es - evaluates current game state"
          "pf n - calculate perft score to depth of n"
          ;;"uci - enable uci mode"
-         "xboard - ebable xboard mode"
+         "xboard - enable CECP mode"
          "quit - quite the Tursas engine"
          ""))
       (let [prot (get-protocol)]
         (cond (= prot :uci) (print-uci-usage)
-              (= prot :xboard) (print-xboard-usage)
+              (= prot :cecp) (print-cecp-usage)
               :else nil))))
 
 (defn process-command
@@ -585,13 +454,13 @@
           "es" (eval-current-state)
           "pf" (display-perft (second words))
           ;;"uci"  (set-repl! :uci)
-          "xboard" (set-protocol! :xboard)
+          "xboard" (set-protocol! :cecp)
           "quit" (quit)
           (case (get-protocol)
                 :general (when-not (empty? (rest words))
                            (recur (rest words)))
                 :uci (process-uci-command words)
-                :xboard (process-xboard-command words)))))
+                :cecp (process-cecp-command words)))))
 
 (defn set-player!
   "Sets the given player's repl.
